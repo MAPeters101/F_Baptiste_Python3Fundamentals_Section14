8214c156936d009322fdca7aeff49891c8593944
Mark Peters <mapeters101@earthlink.net>
2025-03-09 23:35:53 -0400

    Description for exercise03.

---
+++ exercise03.py
@@ -0,0 +1,14 @@
+'''
+Question 3
+Write a function that receives two arguments:
+
+a list of numbers
+a keyword-only argument reverse that defaults to False and indicates an
+ascending sort, but a value of True indicates a descending sort
+Your function should return three values:
+
+a list of the numbers, but sorted (ascending/descending depending on value of
+reverse)
+the minimum value in the list (this is not affected by the value of reverse)
+the maximum value in the list (this is not affected by the value of reverse)
+'''
