184c7c3a6b7b595631ef8967a016fba9ebae85b7
Mark Peters <mapeters101@earthlink.net>
2025-03-09 23:34:54 -0400

    Description for exercise02.

---
+++ exercise02.py
@@ -0,0 +1,15 @@
+'''
+Question 2
+Given the following data:
+
+suits = 'shdc'  # Spades, Hearts, Diamonds, Clubs
+ranks = list('23456789') + ['10', 'J', 'Q', 'K', 'A']
+Write a function that given those two inputs, returns a list with all 52 cards,
+consisting of tuples (rank, suit), i.e.
+
+[
+  [('2', 's'), ('3', 's'), ..., ('K', 's'), ('A', 's')],
+  [('2', 'h'), ('3', 'h'), ..., ('K', 'h'), ('A', 'h')],
+  ...
+]
+'''
