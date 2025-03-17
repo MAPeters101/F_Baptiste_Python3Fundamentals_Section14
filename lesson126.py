b9d94f2c98dfdd02ec5f4eef31062219cd98758b
Mark Peters <mapeters101@earthlink.net>
2025-03-17 17:24:59 -0400

    Work on lesson126 to 2:10.

---
+++ lesson126.py
@@ -0,0 +1,16 @@
+l = [1, 10, 2, 9, 3, 8]
+t = (1, 10, 2, 9, 3, 8)
+s = {1, 10, 2, 9, 3, 8}
+
+print(sorted(l))
+print(sorted(t))
+print(sorted(s))
+print(l)
+print(t)
+print(s)
+print(sorted(l, reverse=True))
+print(sorted(t, reverse=True))
+print(sorted(s, reverse=True))
+print('-'*80)
+
+
