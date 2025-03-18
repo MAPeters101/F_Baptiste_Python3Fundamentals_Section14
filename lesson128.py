8520ecd3321a3cdaf08680f7d1754facee03225c
Mark Peters <mapeters101@earthlink.net>
2025-03-17 21:12:55 -0400

    Work on lesson128 to 1:50.

---
+++ lesson128.py
@@ -0,0 +1,14 @@
+l = [1,2,3,4,5]
+t = (10, 20, 30)
+
+result = zip(l, t)
+print(result)
+print(type(result))
+print(next(result))
+print(next(result))
+print(next(result))
+#print(next(result))
+print('-'*80)
+
+
+
