58dda9904cd172cad9892d40534ae061b77ea711
Mark Peters <mapeters101@earthlink.net>
2025-03-17 21:41:17 -0400

    Work on lesson128 from 5:00 to 9:20.

--- lesson128.py
+++ lesson128.py
@@ -31,7 +31,29 @@ l2 = range(1_000_000)
 combo = list(zip(l1, l2))
 end = perf_counter()
 print(f'elapsed: {end - start}')
+print('='*80)
+
+d = dict([('a', 1), ('b', 2), ('c', 3)])
+print(d)
 print()
+
+data = [
+    ('item1', 10, 100.0),
+    ('item2', 5, 25.0),
+    ('item3', 100, 0.25)
+]
+schema = ('widget', 'num_sold', 'unit_price')
+
+
+
+schema = ('widget', 'manufacturer', 'num_sold', 'unit_price', 'discount')
+d = {}
+for item in data:
+    d[item[0]] = {'num_sold': item[1], 'unit_price': item[2]}
+print(d)
+
+
+
 
 
 
